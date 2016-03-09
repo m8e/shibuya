1ed701b9c3258b2c6c457e30673293bbca6809db
Tal Weinfeld <talw@webwise.co.il>
2016-03-09 09:00:24 +0200

    Return a response

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ var _ = require('lodash'),
     bacon = require('baconjs'),
     EventEmitter = require('events');
 
+var CALL_TIMEOUT = 1000,
+    DELIVERY_MODE_NON_PERSISTENT = 1;
+
 var notImplemented = function(){ throw(new Error('Not Implemented')); };
 
 var Constructor = function(config){
@@ -25,7 +28,7 @@ var Constructor = function(config){
 
     // Create backchannel
     var backchannelStream =  connectionProperty
-        .flatMap(function(connection){ console.log('Creating Backchannel Queue'); return bacon.fromCallback(connection.queue.bind(connection, backchannelId, { exclusive: true })); })
+        .flatMap(function(connection){ return bacon.fromCallback(connection.queue.bind(connection, backchannelId, { exclusive: true })); })
         .flatMap(function(queue){ return bacon.fromBinder(function(sink){ queue.subscribe({ exclusive: true, ack: false }, function(){ sink(_.zipObject(["message", "headers", "deliveryInfo", "messageObject"], arguments)); }); }); });
 
     // Create calling pipeline
@@ -44,16 +47,21 @@ var Constructor = function(config){
             // Send the message out
             call["exchange"].publish(["shibuya", call["serviceIdentifier"]].join('-'), call["options"], {
                 contentType: "application/json",
-                deliveryMode: 1,
+                deliveryMode: DELIVERY_MODE_NON_PERSISTENT,
                 replyTo: backchannelId,
                 correlationId: call["callId"]
             });
 
-            return backchannelStream.filter(function(message){
-                return message["deliveryInfo"]["correlationId"] === call["callId"]
-            }).take(1);
+            return backchannelStream
+                .filter(function(message){ return message["deliveryInfo"]["correlationId"] === call["callId"] })
+                .map(function(message){ return { status: "ok", message: message, call: call }; })
+                .merge(bacon.later(CALL_TIMEOUT, { status: "timeout", call: call }))
+                .take(1)
         })
-        .log();
+        .onValue(function(res){
+            // Return a response to caller
+            res["call"]["callback"]((res["status"] !== "ok" && new Error(res["status"])) || null, res["message"]);
+        });
 };
 
 util.inherits(Constructor, EventEmitter);
