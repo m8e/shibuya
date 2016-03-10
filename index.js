c1271f141e6a9e9b8a4eefa5250127ff69b5ea1c
Tal Weinfeld <talw@webwise.co.il>
2016-03-10 11:15:10 +0200

    Completely refactored

--- index.js
+++ index.js
@@ -1,75 +1,183 @@
-"use strict";
-
 var _ = require('lodash'),
-    util = require('util'),
-    uuid = require('node-uuid'),
-    amqp = require('amqp'),
     bacon = require('baconjs'),
-    EventEmitter = require('events');
+    amqp = require('amqp'),
+    uuid = require('node-uuid');
 
-var CALL_TIMEOUT = 1000,
-    DELIVERY_MODE_NON_PERSISTENT = 1;
+var AMQP_RECONNECT_STRATEGY_LINEAR = "linear",
+    AMQP_RECONNECT_BACKOFF_TIME = 1000,
+    AMQP_EXCHANGE_TYPE_TOPIC = "topic",
+    AMQP_EXCHANGE_DELIVERY_MODE_NON_PERSISTENT = 1,
+    RESPONSE_TIMEOUT = 3000,
+    CLIENT_ID = uuid.v4();
 
-var notImplemented = function(){ throw(new Error('Not Implemented')); };
+module.exports = function(configuration){
 
-var Constructor = function(config){
+    var sharedId = configuration["shared_id"] || "SHIBUYA";
 
-    var backchannelId = _.template('shibuya-backchannel-<%-uuid%>')({ uuid: uuid.v4() }),
-        exchangeId = ["shibuya", config["exchange_id"] || "exchange"].join('-');
+    /** Shared Resources **/
+    // Establish local connection
+    var connectionProperty = (function(connection){
+        return bacon
+            .fromEvent(connection, 'ready')
+            .map(connection)
+            .take(1);
+    })(amqp.createConnection(
+        configuration["connection"],
+        {
+            reconnect: true,
+            reconnectBackoffStrategy: AMQP_RECONNECT_STRATEGY_LINEAR,
+            reconnectBackoffTime: AMQP_RECONNECT_BACKOFF_TIME
+        }
+    ));
 
-    // Create generic connection property
-    var connectionProperty = bacon.fromCallback(function(cb){
-        (function(con){ con.on('ready', _.once(_.partial(cb, con))); })(amqp.createConnection(config["connection"], { reconnect: true }));
-    }).toProperty();
+    // Define/Connect to system-wide exchange (should be shared across all Shibuya's system components)
+    var exchangeProperty = connectionProperty
+        .flatMap(function(connection){
+            return bacon.fromCallback(connection.exchange.bind(
+                connection,
+                [sharedId, "exchange"].join('|'),
+                {
+                    type: AMQP_EXCHANGE_TYPE_TOPIC,
+                    durable: false
+                }
+            ));
+        });
 
-    var exchangeProperty = connectionProperty.flatMap(function(connection){
-        return bacon.fromCallback(connection.exchange.bind(connection, exchangeId, { type: "topic", autoDelete: true, durable: false }));
-    }).toProperty();
+    // Shared feedback channel
+    var feedbackStream = connectionProperty
+        .flatMap(function(connection){ return bacon.fromCallback(connection.queue.bind(connection, [sharedId, "feedback", CLIENT_ID].join('|'), { exclusive: true })); })
+        .doAction(function(queue){
+            queue.bind([sharedId, "exchange"].join('|'), [sharedId, "feedback", CLIENT_ID].join('|'));
+        })
+        .flatMap(function(queue){
+            return bacon.fromBinder(function(sink){
+                queue.subscribe(
+                    { ack: false },
+                    function(message, headers, deliveryInfo, messageObject){
+                        sink({
+                            message: message,
+                            headers: headers,
+                            deliveryInfo: deliveryInfo,
+                            messageObject: messageObject
+                        });
+                    }
+                );
+            });
+        });
 
-    // Create backchannel
-    var backchannelStream =  connectionProperty
-        .flatMap(function(connection){ return bacon.fromCallback(connection.queue.bind(connection, backchannelId, { exclusive: true })); })
-        .flatMap(function(queue){ return bacon.fromBinder(function(sink){ queue.subscribe({ exclusive: true, ack: false }, function(){ sink(_.zipObject(["message", "headers", "deliveryInfo", "messageObject"], arguments)); }); }); });
 
-    // Create calling pipeline
+    feedbackStream.onValue(_.noop);
+
+    var interface = {};
+
+    //** Service Declaration **/
     bacon
         .fromBinder(function(sink){
-            this.call = function(serviceIdentifier, options, callback){
-                sink({
-                    serviceIdentifier: serviceIdentifier,
-                    options: options || {},
-                    callback: callback
-                });
-            };
-        }.bind(this))
-        .combine(exchangeProperty, function(call, exchange){ return _.assign(call, { exchange: exchange, callId: uuid.v4() }); })
-        .flatMap(function(call){
-            // Send the message out
-            call["exchange"].publish(["shibuya", call["serviceIdentifier"]].join('-'), call["options"], {
-                contentType: "application/json",
-                deliveryMode: DELIVERY_MODE_NON_PERSISTENT,
-                replyTo: backchannelId,
-                correlationId: call["callId"]
-            });
-
-            return backchannelStream
-                .filter(function(message){ return message["deliveryInfo"]["correlationId"] === call["callId"] })
-                .map(function(message){ return { status: "ok", message: message, call: call }; })
-                .merge(bacon.later(CALL_TIMEOUT, { status: "timeout", call: call }))
-                .take(1)
+            interface.register = function(serviceName, instance){ sink({ serviceName: serviceName, instance: instance }); };
+            return function(){ interface.register = function(){ throw(new Error('Method no longer available')); } };
         })
-        .onValue(function(res){
-            // Return a response to caller
-            res["call"]["callback"]((res["status"] !== "ok" && new Error(res["status"])) || null, res["message"]);
+        .flatMap(function(registrationRequest){
+            var serviceIdentifier = [sharedId, "service", registrationRequest["serviceName"]].join('|'),
+                instance = registrationRequest["instance"];
+            return connectionProperty
+                .flatMap(function(connection){
+                    return bacon.fromCallback(connection.queue.bind(
+                        connection,
+                        serviceIdentifier,
+                        {}
+                    ));
+                })
+                .combine(exchangeProperty, function(queue, exchange){
+                    queue.bind(exchange, serviceIdentifier);
+                    return queue;
+                })
+                .flatMap(function(queue){
+                    return bacon.fromBinder(function(sink){ return queue.subscribe({ ack: false }, function(message, headers, deliveryInfo, messageObject){
+                            sink({
+                                message: message,
+                                headers: headers,
+                                deliveryInfo: deliveryInfo,
+                                messageObject: messageObject
+                            });
+                        });
+                    });
+                })
+                .flatMap(function(remoteCall){
+
+                    return bacon
+                        .fromNodeCallback(instance[remoteCall.message["method"]].bind(instance, remoteCall.message["options"]))
+                        .map(function(data){
+                            return {
+                                status: "ok",
+                                payload: data
+                            };
+                        })
+                        .mapError(function(error){
+                            return {
+                                status: "error",
+                                payload: error
+                            };
+                        })
+                        .map(function(obj){
+                            return _.assign(obj, {
+                                correlation_id: remoteCall.deliveryInfo["correlationId"],
+                                reply_to: remoteCall.deliveryInfo["replyTo"]
+                            });
+                        })
+                });
+        })
+        .combine(exchangeProperty, function(response, exchange){
+            return { response: response, exchange: exchange };
+        })
+        .onValue(function(obj){
+            var exchange = obj["exchange"],
+                response = obj["response"];
+
+            exchange.publish(response["reply_to"], _.pick(response, 'status', 'payload'), {
+                deliveryMode: AMQP_EXCHANGE_DELIVERY_MODE_NON_PERSISTENT,
+                correlationId: response["correlation_id"]
+            });
         });
+
+    /** Consumer **/
+    bacon.fromBinder(function(sink){
+        interface.remoteCall = function(serviceName, methodName, options, callback){
+            sink({
+                serviceName: serviceName,
+                methodName: methodName,
+                options: options,
+                callback: callback || _.noop,
+                id: uuid.v4()
+            });
+        };
+    })
+    .combine(exchangeProperty, function(call, exchange){
+        return _.assign(call, { exchange: exchange });
+    })
+    .flatMap(function(call) {
+        call.exchange.publish(
+            [sharedId, "service", call.serviceName].join('|'),
+            {
+                method: call.methodName,
+                options: call.options
+            },
+            {
+                replyTo: [sharedId, "feedback", CLIENT_ID].join('|'),
+                correlationId: call.id
+            }
+        );
+
+        return feedbackStream
+            .filter(function(o){ return o.deliveryInfo["correlationId"] === call.id; })
+            .map('.message')
+            .merge(bacon.later(RESPONSE_TIMEOUT, { status: "error", payload: new Error('Failed to obtain response in a timely fashion') }))
+            .take(1)
+            .map(function(o){ return _.assign(o, { callback: call["callback"] }); });
+
+    })
+    .onValue(function(message){
+        message.callback((message.status !== "ok" || null) && new Error(message.payload), message.payload);
+    });
+
+    return interface;
 };
-
-util.inherits(Constructor, EventEmitter);
-
-_.assign(Constructor.prototype, {
-    call: notImplemented,
-    register: notImplemented
-});
-
-module.exports = Constructor;
-
