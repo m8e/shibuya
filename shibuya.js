9d0410c670f232c97ef1b50bca15386ab71d7f43
Tal Weinfeld <talw@webwise.co.il>
2016-03-20 09:04:49 +0200

    Rewrite

--- shibuya.js
+++ shibuya.js
@@ -24,9 +24,8 @@ module.exports = function(userConfiguration){
         DEFAULT_NAMESPACE_ID = "shibuya",
         CLIENT_ID = uuid.v4();
 
-    var nameTemplate = function(name, id){
-        return _.compact([DEFAULT_NAMESPACE_ID, name, id]).join('.');
-    };
+    // Rabbit entity template
+    var nameTemplate = function(name, id){ return _.compact([DEFAULT_NAMESPACE_ID, name, id && murmurhash.v3(id).toString()]).join('.'); };
 
     // Global configuration
     var config = _.defaultsDeep(userConfiguration, {
@@ -40,177 +39,124 @@ module.exports = function(userConfiguration){
         }
     });
 
-    // Global connection
-    var connectionProperty = (function(connection){
-        return Bacon
-            .fromEvent(connection, 'ready', _.constant(connection))
-            .take(1)
-            .toProperty();
-    })(amqp.createConnection(
+    // Create connection
+    var connection = amqp.createConnection(
         config.connection,
         {
             reconnect: true,
             reconnectBackoffStrategy: AMQP_DEFAULT_RECONNECT_STRATEGY,
             reconnectBackoffTime: AMQP_DEFAULT_RECONNECT_BACKOFF_TIME
         }
-    ));
+    );
 
-    // Global exchange
-    var exchangeProperty = connectionProperty.flatMap(function(connection){
-        return Bacon.fromCallback(connection.exchange.bind(
-            connection,
-            nameTemplate('exchange'),
-            {
-                autoDelete: true,
-                type: AMQP_EXCHANGE_TYPE_TOPIC
-            }
-        ));
-    }).toProperty();
+    // Define interface methods
+    var interface = {};
 
-    // Feedback queue
-    var feedbackProperty = connectionProperty.flatMap(function(connection){
-        return Bacon.fromCallback(
-            connection.queue.bind(
-                connection,
-                nameTemplate('feedback', murmurhash.v3(CLIENT_ID).toString()),
-                { exclusive: true }
-            )
-        )
-        .combine(exchangeProperty, function(queue, exchange){
-            return {
-                queue: queue,
-                exchange: exchange
-            };
+    var registerStream = Bacon.fromBinder(function(sink){
+        interface.register = function(name, instance, options){
+            sink({ name: name, instance: instance, options: options });
+        }
+    }).onValue(_.noop);
+
+    var callStream = Bacon.fromBinder(function(sink){
+        interface.call = function(){
+            var args = _.toArray(arguments),
+                cb = args.pop() || _.noop,
+                name = args.shift(),
+                options = args.shift() || {};
+
+            sink({ name: name, cb: cb, options: options });
+        };
+    }).onValue(_.noop);
+
+    Bacon
+        .fromEvent(connection, 'ready')
+        .take(1)
+        .map(connection)
+        .flatMap(function(connection){
+            return Bacon.combineTemplate({
+                connection: connection,
+                exchange: Bacon.fromCallback(connection.exchange.bind(connection,  nameTemplate('exchange'), { autoDelete: true, type: AMQP_EXCHANGE_TYPE_TOPIC })),
+                feedbackQueue: Bacon.fromCallback(connection.queue.bind(connection, nameTemplate('feedback', CLIENT_ID), { exclusive: true }))
+            });
         })
-        .flatMap(function(opts){
-            return Bacon.fromCallback(opts.queue.bind.bind(opts.queue, opts.exchange.name, opts.queue.name)).map(opts.queue);
-        })
-        .flatMap();
-    }).toProperty()
+        .onValue(function(asset){
+            var connection = asset["connection"],
+                exchange = asset["exchange"],
+                feedbackQueue = asset["feedbackQueue"];
 
-    return {
-        registerService: (function(receiver){
-            Bacon.combineTemplate({
-                connection: connectionProperty,
-                exchange: exchangeProperty,
-                params: Bacon.fromBinder(function(sink){
-                    receiver = function(name, service){
-                        sink({
-                            serviceId: nameTemplate('service', murmurhash.v3(name).toString()),
-                            service: service
-                        });
-                    };
-                })
-            })
-            .flatMap(function(params){
-                var serviceId = params.params.serviceId,
-                    service = params.params.service,
-                    connection = params.connection,
-                    exchange = params.exchange;
+            // Bind feedback queue to exchange (the queue name doubles as the routing key)
+            feedbackQueue.bind(exchange.name, feedbackQueue.name);
 
-                return Bacon
-                    .fromCallback(connection.queue.bind(connection, serviceId, { autoDelete: true }))
-                    .flatMap(function(queue){
-                        return Bacon
-                            .fromCallback(queue.bind.bind(queue, exchange.name, serviceId))
-                            .flatMap(function(){
-                                return Bacon
-                                    .fromBinder(
-                                        _.flow(
-                                            function(sink){
-                                                return function(message, header, delivery){
-                                                    sink(delivery["contentType"] === "application/json" && message && _.assign(
-                                                        {
-                                                            method: message["method"],
-                                                            parameters: message["parameters"]
-                                                        },
-                                                        _.pick(delivery, 'correlationId', 'replyTo')
-                                                    ));
-                                                };
-                                            },
-                                            queue.subscribe.bind(queue, { ack: false })
+            return registerStream
+                .flatMap(function(registrationProperties){
+                    var name = registrationProperties["name"],
+                        instance = registrationProperties["instance"],
+                        options = registrationProperties["options"];
+
+                    return Bacon
+                        .fromCallback(connection.queue.bind(connection, nameTemplate('service', name), { autoDelete: true }))
+                        .flatMap(function(queue){
+                            return Bacon
+                                .fromCallback(queue.bind.bind(queue, exchange.name, name))
+                                .flatMap(function(){
+                                    return Bacon
+                                        .fromBinder(
+                                            _.flow(
+                                                function(sink){
+                                                    return function(message, header, delivery){
+                                                        sink(delivery["contentType"] === "application/json" && message && _.assign(
+                                                                {
+                                                                    method: message["method"],
+                                                                    parameters: message["parameters"]
+                                                                },
+                                                                _.pick(delivery, 'correlationId', 'replyTo')
+                                                            ));
+                                                    };
+                                                },
+                                                queue.subscribe.bind(queue, { ack: false })
+                                            )
                                         )
-                                    )
-                                    .filter(Boolean);
-                            });
-                    })
-                    .flatMap(function(invocationRequest){
-                        return Bacon.fromNodeCallback(function(cb){
-                            try {
-                                service[invocationRequest["method"]].apply(service, _.flatten([invocationRequest["parameters"], cb]));
-                            } catch(e) {
-                                cb(e.toString());
-                            }
+                                        .filter(Boolean);
+                                });
                         })
-                        .merge(Bacon.later(DEFAULT_RPC_INVOCATION_RESPONSE_TIMEOUT, new Bacon.Error('Invocation did not yield a response in a timely fashion')))
-                        .map(function(result){
-                            return {
-                                status: STATUS_OK,
-                                result: result
-                            };
-                        })
-                        .mapError(function(error){
-                            return {
-                                status: STATUS_ERROR,
-                                result: error
-                            };
-                        })
-                        .take(1)
-                        .map(function(message){
-                            return exchange.publish.bind(
-                                exchange,
-                                invocationRequest.replyTo,
-                                message,
-                                {
-                                    deliveryMode: AMQP_EXCHANGE_DELIVERY_MODE_NON_PERSISTENT,
-                                    correlationId: invocationRequest.correlationId
-                                }
-                            );
+                        .flatMap(function(invocationRequest){
+                            return Bacon.fromNodeCallback(function(cb){
+                                    try {
+                                        instance[invocationRequest["method"]].apply(instance, _.flatten([invocationRequest["parameters"], cb]));
+                                    } catch(e) {
+                                        cb(e.toString());
+                                    }
+                                })
+                                .merge(Bacon.later(DEFAULT_RPC_INVOCATION_RESPONSE_TIMEOUT, new Bacon.Error('Invocation did not yield a response in a timely fashion')))
+                                .map(function(result){
+                                    return {
+                                        status: STATUS_OK,
+                                        result: result
+                                    };
+                                })
+                                .mapError(function(error){
+                                    return {
+                                        status: STATUS_ERROR,
+                                        result: error
+                                    };
+                                })
+                                .take(1)
+                                .map(function(message){
+                                    return exchange.publish.bind(
+                                        exchange,
+                                        invocationRequest.replyTo,
+                                        message,
+                                        {
+                                            deliveryMode: AMQP_EXCHANGE_DELIVERY_MODE_NON_PERSISTENT,
+                                            correlationId: invocationRequest.correlationId
+                                        }
+                                    );
+                                });
                         });
-                    });
-            })
-            .onValue('.call');
-
-            return receiver;
-        })(),
-        callService: (function(receiver){
-            Bacon.combineTemplate({
-                connection: connectionProperty,
-                exchange: exchangeProperty,
-                call: Bacon.fromBinder(function(sink){
-                    receiver = function(name, method, parameters, callback){
-                        sink({
-                            serviceId: nameTemplate('service', murmurhash.v3(name).toString()),
-                            method: method,
-                            parameters: parameters || [],
-                            callback: callback || _.noop,
-                            id: uuid.v4()
-                        });
-                    };
                 })
-            })
-            .flatMap(function(opts){
-                Bacon
-                    .fromCallback(
-                        opts.exchange.publish.bind(
-                            opts.exchange,
-                            opts.call.serviceId,
-                            _.pick(opts.call, 'method', 'parameters'),
-                            {
-                                deliveryMode: AMQP_EXCHANGE_DELIVERY_MODE_NON_PERSISTENT,
-                                correlationId: opts.call.id,
-                                replyTo: opts.queue.name
-                            }
-                        )
-                    )
-                    .flatMap(function(){
+                .onValue('.call');
+        });
 
-                    })
-
-            })
-            .log();
-
-            return receiver;
-        })()
-    };
+    return interface;
 };
